fa7af208273b2eaa6182cd9e209433e92252062d
Alexandria P. <alxpettit@gmail.com>
2024-09-11 16:11:30 -0600

    first

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A Nix flake for Vroid Studio";
+
+  inputs.erosanix.url = "github:emmanuelrosa/erosanix";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    erosanix,
+  }: {
+    packages.x86_64-linux = let
+      pkgs = import "${nixpkgs}" {
+        system = "x86_64-linux";
+      };
+    in
+      with (pkgs // erosanix.packages.x86_64-linux // erosanix.lib.x86_64-linux); {
+        default = self.packages.x86_64-linux.vroid-studio;
+
+        vroid-studio = pkgs.callPackage ./vroid-studio.nix {
+          inherit mkWindowsApp makeDesktopIcon copyDesktopIcons;
+
+          wine = wineWowPackages.full;
+        };
+      };
+
+    apps.x86_64-linux.vroid-studio = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.vroid-studio}/bin/vroid-studio";
+    };
+
+    apps.x86_64-linux.default = self.apps.x86_64-linux.vroid-studio;
+  };
+}
